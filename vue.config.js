608365c365f3ee802dd8006ae7e496514bd38000
Linnnsyuebaba <yuebaba1431@gmail.com>
2020-06-13 23:06:32 +0800

    fix 修复本地代理端口

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === 'production' ? '/xmind-recruitment-project' : '/',
   transpileDependencies: ['vuetify'],
   devServer: {
     proxy: {
       '/xmind': {
-        target: 'http://127.0.0.1:4000',
+        target: 'http://127.0.0.1:2222',
         changeOrigin: true, //是否跨域
         ws: true, //是否启用websockets
         pathRewrite: {
